6afa45fc74f4e96c9c2ea7c96d39fd22f2fb6710
ZeMunchkin <smbrandes17@gmail.com>
2018-01-30 12:40:25 -0800

    have a bug in our ajax get request

--- src/collections/videos.js
+++ src/collections/videos.js
@@ -2,6 +2,26 @@ var Videos = Backbone.Collection.extend({
 
   model: Video,
 
+  search: function(query) {
+    Backbone.ajax({
+      url: 'https://www.googleapis.com/youtube/v3/search',
+      type: 'GET',
+      data: JSON.stringify({
+        'q': query,
+        'maxResults': '5',
+        'videoEmbeddable': true,
+        'key': window.YOUTUBE_API_KEY,
+        
+      }),
+      // contentType: ,
+      success: function(data) {
+        console.log(data);
+      },
+      error: function(data) {
+        console.log('FAILED', data);
+      }
+    });
+  }
 });
 
 
--- src/views/search.js
+++ src/views/search.js
@@ -6,6 +6,26 @@ var SearchView = Backbone.View.extend({
     return this;
   },
 
-  template: templateURL('src/templates/search.html')
+  template: templateURL('src/templates/search.html'),
 
+  events: {
+    'click button': 'searchHandler',
+    'keypress': 'updateOnEnter'
+  },
+  
+  updateOnEnter: function (e) {
+    if (e.keyCode === 13) {
+      this.searchHandler();
+    }
+  },
+  
+  //search handler function
+  searchHandler: function() {
+    // set variable to hold input (escape this)
+    var query = _.escape($('.form-control').val());
+    // call the videos api request passing in the string
+    this.collection.search(query);
+    //empty the form
+    $('.form-control').val('');
+  }
 });
