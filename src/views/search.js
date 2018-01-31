e5ed8220f79ff068fa078bbf253d04fb2e88c35b
ZeMunchkin <smbrandes17@gmail.com>
2018-01-30 17:18:37 -0800

    Revert "have live searches (advanced content)"
    This reverts commit 17feb1c94cdad3cb9f734444155f5196701764a4.

--- src/views/search.js
+++ src/views/search.js
@@ -10,28 +10,22 @@ var SearchView = Backbone.View.extend({
 
   events: {
     'click button': 'searchHandler',
-    'keyup input': 'searchHandler'
+    'keyup input': 'updateOnEnter'
   },
   
-  // updateOnEnter: function (e) {
-  //   if (e.keyCode === 13) {
-  //     this.searchHandler();
-  //   }
-  // },
+  updateOnEnter: function (e) {
+    if (e.keyCode === 13) {
+      this.searchHandler();
+    }
+  },
   
   //search handler function
-  searchHandler: function(e) {
-    console.log(e);
+  searchHandler: function() {
     // set variable to hold input (escape this)
     var query = _.escape($('.form-control').val());
     // call the videos api request passing in the string
     this.collection.search(query);
     //empty the form
-    //if event is enter or mouseclick
-    if (e.keyCode === 13 || e.type === 'click') {
-      //clear the input
-      $('.form-control').val('');
-      
-    }
+    $('.form-control').val('');
   }
 });
